1fce3b54a805babb31895d8e33fae3739767aac7
jwu6492 <yiwu1432@gmail.com>
2021-03-28 14:26:49 -0400

    edit plugin

--- src/plugins/jonathan/plugin.js
+++ src/plugins/jonathan/plugin.js
@@ -1,9 +1,10 @@
-
-app.message(/^(hi|hola|hey|bot).*/, async ({ context, say }) => {
-  const greeting = context.matches[0];
-  if (greeting == 'bot') {
-    await say(`hi <@${message.user}>, how are you?`);
-  } else {
-    await say(`${greeting} <@${message.user}>, how are you?`);
-  }
-});
+export default async (app) => {
+  app.message(/^(hi|hola|hey|bot).*/, async ({ context, say }) => {
+    const greeting = context.matches[0];
+    if (greeting === 'bot') {
+      await say(`hi <@${message.user}>, how are you?`);
+    } else {
+      await say(`${greeting} <@${message.user}>, how are you?`);
+    }
+  });
+};
