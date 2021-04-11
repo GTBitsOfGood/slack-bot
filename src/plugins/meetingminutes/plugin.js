eebfcce588cdc5e1ffb9fcd0a39739fdeb864654
cmiller615 <carsenjakemiller@gmail.com>
2021-04-11 00:24:12 -0400

    fetch pathing fix attempt

--- src/plugins/meetingminutes/plugin.js
+++ src/plugins/meetingminutes/plugin.js
@@ -6,12 +6,15 @@ export default async (app) => {
     await say(msg_events);
   }));
 }
-let msg_opener = `Extra extra! Read all about it! Here are the most recent notes from the ` + date + ` ` + title + ` :rolled_up_newspaper:`;
-let msg_tldr = `TLDR: ` + tldr;
-let msg_events = `Events: ` + events;
-let title, date, tldr, events;
-//TODO fix fetch - see bug.txt
-fetch("./data/meeting.json")
+var msg_opener = `Extra extra! Read all about it! Here are the most recent notes from the ` + date + ` ` + title + ` :rolled_up_newspaper:`;
+var msg_tldr = `TLDR: ` + tldr;
+var msg_events = `Events: ` + events;
+var title, date, tldr, events;
+
+//TODO fix fetch pathing - see bug.txt
+var path = require("src/plugins/meetingminutes/meeting.json");
+var absolute_path = path.resolve("src/plugins/meetingminutes/meeting.json");
+fetch(absolute_path)
   .then(function(resp) {
     return resp.json();
   })
